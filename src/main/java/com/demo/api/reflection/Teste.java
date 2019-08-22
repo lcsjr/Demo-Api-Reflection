067ef7c512d859b686c6482ac91325b5625d859d
Luiz Santos <luiz.santos@thomsonreuters.com>
2019-08-22 16:58:47 -0300

    implementacoes de buildInsert na Metamodel

---
+++ src/main/java/com/demo/api/reflection/Teste.java
@@ -0,0 +1,41 @@
+package com.demo.api.reflection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import lombok.Getter;
+
+public class Teste {
+
+	private static List<Pessoa> pessoas =  new ArrayList<>();
+	
+	static {
+		pessoas.add(new Pessoa("Luiz",35));
+		pessoas.add(new Pessoa("Cinthia",34));
+		pessoas.add(new Pessoa("Davi",5));
+		pessoas.add(new Pessoa("Hilda",62));
+	}
+
+	public static void main(String[] args) {
+
+		List<String> nomes = pessoas.stream().map( Pessoa::getNome).collect(Collectors.toList());
+		nomes.forEach(System.out::println);
+		nomes.add(0, "Luiz Santos");
+		System.out.println(" ");
+		nomes.forEach(System.out::println);
+	}
+}
+
+@Getter
+class Pessoa {
+	
+	String nome;
+	int age;
+
+	public Pessoa(String nome, int age) {
+		this.nome = nome;
+		this.age = age;
+	}
+
+}
--- src/main/java/com/demo/api/reflection/util/Metamodel.java
+++ src/main/java/com/demo/api/reflection/util/Metamodel.java
@@ -3,6 +3,8 @@ package com.demo.api.reflection.util;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import com.demo.api.reflection.annotation.Column;
 import com.demo.api.reflection.annotation.PrimaryKey;
@@ -22,38 +24,40 @@ public class Metamodel {
 	public PrimaryKeyField getPrimaryKey() {
 		
 		Field[] Fields = clss.getDeclaredFields();
-		
 		for (Field field : Fields){
 			PrimaryKey annotation = field.getAnnotation(PrimaryKey.class);
-			if(annotation != null) {
-				PrimaryKeyField primaryKeyField = new PrimaryKeyField(field);
-				
-				return primaryKeyField;
-			}
+			if(annotation != null) 
+				return new PrimaryKeyField(field);
 		}
-		
 		throw new IllegalArgumentException("No primary key found in class " + clss.getSimpleName());
 	}
 
 	public List<ColumnField> getColumns() {
 		
 		List<ColumnField> columnsFields = new ArrayList<>();
-		
 		Field[] Fields = clss.getDeclaredFields();
 		for (Field field : Fields){
 			Column annotation = field.getAnnotation(Column.class);
-			if(annotation != null) {
-				ColumnField column = new ColumnField(field);
-				columnsFields.add(column);
-			}
+			if(annotation != null) 
+				columnsFields.add(new ColumnField(field));
 		}
-		
 		return columnsFields;
 	}
 
 	public String buildInsertRequest() {
-		
-		return null;
+		return "insert into " + this.clss.getSimpleName() + " ("+buildColumnNames()+") values (" + buildQuestionMarksElement() + ")" ;
+	}
+
+	private String buildQuestionMarksElement() {
+		int numberOfColumns = getColumns().size()+1;
+		return IntStream.range(0, numberOfColumns).mapToObj( index -> "?").collect(Collectors.joining(", "));
+	}
+
+	private String buildColumnNames() {
+		String primeryKeyColumnName = getPrimaryKey().getName();
+		List<String> columnNames = getColumns().stream().map(ColumnField::getName).collect(Collectors.toList());
+		columnNames.add(0, primeryKeyColumnName);
+		return String.join(", ", columnNames);
 	}
 
 }
